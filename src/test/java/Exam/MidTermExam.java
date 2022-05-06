12d257c5c0d78021f327f220c0f9e0b498a93f16
Fortesa Nikci <fortesanikci@Fortesas-MacBook-Pro.local>
2022-05-06 18:56:24 -0400

    text

---
+++ src/test/java/Exam/MidTermExam.java
@@ -0,0 +1,90 @@
+package Exam;
+
+public class MidTermExam {
+
+    /**
+     * Ques-2:
+     * Verify feelsLike-temp value is in between the low-temp value and high-temp value
+     * Web: https://www.darksky.net/
+     * points: 20
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /**
+         * Ques-1:
+         * Create a method to return factorial value of input-int-value
+         * points: 10
+         */
+        /*
+         *
+         * eg:
+         * input -> 5
+         * returned value -> (5*4*3*2*1) = 120
+         *
+         * input -> 1
+         * returned value -> (1) = 1
+         *
+         * input -> 3
+         * returned value -> (3*2*1) = 6
+         *
+         * input -> 10
+         * returned value -> (10*9*8*7*6*5*4*3*2*1) =
+         */
+
+        /**
+         * Ques-3:
+         * Create a method to return the common Strings values in two input-String-arrays
+         * points: 30
+         * Note: two input-String-arrays can be of same size or different size
+         */
+        /*
+         *
+         * eg:
+         * input -> {"happY", "king", "peace", "living standard"} , {"king kong", "Happy", "PEACE"}
+         * returned value -> [happy, peace]
+         *
+         * input -> {“ab”, “abcd”, “abc”, “abcde”, “defg”, "koli”} , {“abcde”, “jhuy”, “plot”, “koli”}
+         * returned value -> [abcde, koli]
+         *
+         */
+
+    /**
+     * Ques-4:
+     * Create a method to return missing smallest positive integer (greater than 0) from given int-array.
+     * points: 40
+     */
+    /*
+     *
+     * eg:
+     * 	input -> : {3, 5, 1, 4, 2, 7}
+     * 	returned value : 6
+     *
+     * 	input -> : {3, 5, 1, 4, 2}
+     * 	returned value : 6
+     *
+     * 	input -> {2, 5, -1, 0, 6}
+     * 	returned value : 1
+     *
+     * 	input -> {0, 5, -1, 1, 2, 5, 3, 7, 1, 2}
+     * 	returned value : 4
+     *
+     * 	input -> {-2, -5, -1, -10, -6}
+     * 	returned value : 1
+     *
+     */
+
+
+
+}
